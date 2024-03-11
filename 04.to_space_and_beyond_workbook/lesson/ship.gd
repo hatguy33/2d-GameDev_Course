f0df57c109e3493c9605dbb33c6365e45de5f899
hatguy33 <maderas@s.sfusd.edu>
2024-03-11 12:04:00 -0700

    L.4

--- 04.to_space_and_beyond_workbook/lesson/ship.gd
+++ 04.to_space_and_beyond_workbook/lesson/ship.gd
@@ -1,11 +1,30 @@
 extends Sprite2D
 
+var boost_speed := 1500.0
+var normal_speed := 600.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var max_speed := normal_speed
+var velocity := Vector2(0, 0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(delta: float) -> void:
+	var direction := Vector2(0, 0)
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
+
+	velocity = direction * max_speed
+	position += velocity * delta
+
+	if direction.length() > 0.0:
+		rotation = velocity.angle()
+
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
